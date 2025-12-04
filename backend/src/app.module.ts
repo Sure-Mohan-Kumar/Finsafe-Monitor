2ae38a817cfb3bdea9c7572900470b16f37e345c
Sure-Mohan-Kumar <suremohan66@gmail.com>
2025-12-04 13:31:54 +0530

    feat(db): add User and Transaction entities and configure TypeORM entities loading

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,18 +2,19 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config'; 
 import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres', 
-      database: 'postgres',
-      autoLoadEntities: true,
-      synchronize: true,    
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'postgres',
+    database: 'postgres',
+    synchronize: true,
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
     }),
   ],
   controllers: [AppController],
